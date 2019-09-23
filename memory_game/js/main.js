23dbd4e610ce9750d735703419ba2e1ab57d8070
Kathrin Eichinger <kathrin801@gmail.com>
2019-09-23 15:59:10 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,51 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
+
+
+
+
 
 // Function for checking whether selected cards match
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	}
 	else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
 
 // Placing cards flipped into cards in play set
 function flipCard(cardID) {
 
-	console.log("User flipped "+cards[cardID]);
+	console.log("User flipped "+cards[cardID].rank);
 
-	cardsInPlay.push(cards[cardID]);
+	cardsInPlay.push(cards[cardID].rank);
+
+	console.log(cards[cardID].cardImage)
+	console.log(cards[cardID].suit)
 
 	// Check if two cards are a match
 	if (cardsInPlay.length === 2) {
