d4d472497035084773e7581c2e78d892f7f07c8c
Kathrin Eichinger <kathrin801@gmail.com>
2019-09-23 12:38:43 +0100

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,30 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-// First card the user flipped
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-// Second card the user flipped
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-// Check if two cards are a match
-if (cardsInPlay.length === 2) {
+// Function for checking whether selected cards match
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	}
 	else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
 }
+
+// Placing cards flipped into cards in play set
+function flipCard(cardID) {
+
+	console.log("User flipped "+cards[cardID]);
+
+	cardsInPlay.push(cards[cardID]);
+
+	// Check if two cards are a match
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+
+}
+
+
+flipCard(0);
+flipCard(2);
